f53753ae2583a4473daa22f8edc1e5255268de5b
진은정 <1104_jin@naver.com>
2022-04-14 13:18:11 +0900

    feature/backend setting

---
+++ Server/wiselaundry/src/main/java/com/ssafy/wiselaundry/domain/user/db/repository/UserRepositorySpp.java
@@ -0,0 +1,23 @@
+package com.ssafy.wiselaundry.domain.user.db.repository;
+
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import com.ssafy.wiselaundry.domain.user.db.entity.QUser;
+import com.ssafy.wiselaundry.domain.user.db.entity.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class UserRepositorySpp {
+    @Autowired
+    private JPAQueryFactory jpaQueryFactory;
+    QUser quser = QUser.user;
+
+
+    public User findByEmail(String userEmail){
+        return jpaQueryFactory.select(quser).from(quser)
+                .where(quser.userEmail.eq(userEmail)).fetchOne();
+    };
+
+
+
+}
