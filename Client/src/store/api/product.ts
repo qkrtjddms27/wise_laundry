aee023ffadb8a6b66af2b95f30574a6de8af6aec
이혜진 <hy2jinl22@gmail.com>
2022-04-16 00:26:57 +0900

    fix/front setting

--- Client/src/store/api/product.ts
+++ Client/src/store/api/product.ts
@@ -1,8 +1,8 @@
 import axios from "axios"
 
 const apiClient = axios.create({
-  // baseURL: "https://j6e106.p.ssafy.io/api",
-  baseURL: "http://localhost:8080/api",
+  baseURL: "https://j6e106.p.ssafy.io/api",
+  // baseURL: "http://localhost:8080/api",
   // baseURL: "https://j6e106.p.ssafy.io/api",
   headers: {
     "Content-type": "application/json",
