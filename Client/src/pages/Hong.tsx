394c0d75129b9c1e500593e9039ac10ef2096556
이혜진 <hy2jinl22@gmail.com>
2022-04-15 20:44:05 +0900

    chore/Fix frontend settings

--- Client/src/pages/Hong.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const Hong = () => {
-  return (
-    <div>Hong</div>
-  )
-}
-
-export default Hong
--- Client/src/pages/Jin.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const Jin = () => {
-  return (
-    <div>Jin</div>
-  )
-}
-
-export default Jin
