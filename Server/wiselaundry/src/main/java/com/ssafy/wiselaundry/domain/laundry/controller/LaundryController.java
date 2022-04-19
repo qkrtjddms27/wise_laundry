1e0b6d4f8a6a53ce81a9c2b3545ac056249c2985
박성은 <qkrtjddms27@naver.com>
2022-04-19 15:34:18 +0900

    fix/UserController

--- Server/wiselaundry/src/main/java/com/ssafy/wiselaundry/domain/laundry/controller/LaundryController.java
+++ Server/wiselaundry/src/main/java/com/ssafy/wiselaundry/domain/laundry/controller/LaundryController.java
@@ -1,2 +1,13 @@
-package com.ssafy.wiselaundry.domain.laundry.controller;public class LaundryController {
+package com.ssafy.wiselaundry.domain.laundry.controller;
+
+import io.swagger.annotations.Api;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Api("유저 API")
+@Slf4j
+@RestController
+@RequestMapping("/api/laundry")
+public class LaundryController {
 }
