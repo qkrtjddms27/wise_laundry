055e824f25304b67c0ad7c1ef3cf9e052562253a
bbnerino <bbnerino@gmail.com>
2022-04-21 00:32:53 +0900

    feat/mylaundry page, detail

---
+++ Client/src/pages/laundryBoard/dummy.js
@@ -0,0 +1,64 @@
+export const items =[
+  {
+    laundryId: 13,
+    careLabel: ['세탁불가','다리미','물빨래','손세탁'],
+    laundryImg: 'http://image.mujikorea.net/goods/00/06/76/93/W1AA026_COL_03_400.jpg',
+  },
+  {
+    laundryId: 21,
+    careLabel: ['세탁불가','다리미','물빨래','손세탁','허리업','비싸요'],
+    laundryImg: 'https://image.msscdn.net/images/goods_img/20200928/1629444/1629444_1_500.jpg',
+  },
+  {
+    laundryId: 32,
+    careLabel: ['세탁불가','다리미','물빨래','손세탁'],
+    laundryImg: 'https://t1.daumcdn.net/cfile/tistory/21627C4F54E6D94D1D',
+  },
+  {
+    laundryId: 45,
+    careLabel: ['세탁불가','다리미','물빨래','손세탁','허리업','비싸요'],
+    laundryImg: 'https://shopthekaia.com/web/product/tiny/202010/f253204d7a76836e35987f877ccd1413.jpg',
+  },
+  {
+    laundryId: 57,
+    careLabel: ['세탁불가','다리미','물빨래','손세탁'],
+    laundryImg: 'http://m.daisybird.co.kr/web/product/big/201512/54_shop1_853996.jpg',
+  }
+]
+
+export const items2 =[
+  {
+    laundryId: 512,
+    careLabel: ['세탁불가','다리미','물빨래','손세탁','허리업','비싸요'],
+    laundryImg: 'http://image.mujikorea.net/goods/00/06/76/93/W1AA026_COL_03_400.jpg',
+  },
+  {
+    laundryId: 124,
+    careLabel: ['세탁불가','다리미','물빨래','손세탁','허리업','비싸요'],
+    laundryImg: 'https://image.msscdn.net/images/goods_img/20200928/1629444/1629444_1_500.jpg',
+  },
+  {
+    laundryId: 542,
+    careLabel: ['세탁불가','다리미','물빨래','손세탁'],
+    laundryImg: 'https://t1.daumcdn.net/cfile/tistory/21627C4F54E6D94D1D',
+  },
+  {
+    laundryId: 635,
+    careLabel: ['세탁불가','다리미','물빨래','손세탁','허리업','비싸요'],
+    laundryImg: 'https://shopthekaia.com/web/product/tiny/202010/f253204d7a76836e35987f877ccd1413.jpg',
+  },
+  {
+    laundryId: 123,
+    careLabel: ['세탁불가','다리미','물빨래','손세탁'],
+    laundryImg: 'http://m.daisybird.co.kr/web/product/big/201512/54_shop1_853996.jpg',
+  }
+]
+
+export const detailitem = {
+  laundryId: 12, 
+  careLabel: ['세탁불가','다리미','물빨래','손세탁','허리업','비싸요'], 
+  laundryInfo: ['폴로','스웨터','꽈배기','울니트','1992'], 
+  laundryImg: 'http://m.daisybird.co.kr/web/product/big/201512/54_shop1_853996.jpg' ,
+  laundryOwnerNick: '김순미',
+  laundryOwnerId: 5 
+}
