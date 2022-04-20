4997188c069c24bcf279a7b43c20255873560cf8
이혜진 <hy2jinl22@gmail.com>
2022-04-20 10:18:53 +0900

    fix/semantic tag

--- Client/src/store/state/theme.ts
+++ Client/src/store/state/theme.ts
@@ -1,28 +1,35 @@
 import { atom } from "recoil"
 
 // 1번 타입을 먼저 등록을 해준다 . 🎨
-interface Itheme{
-  bgColor:string
-  fontColor:string
-  navColor:string
+interface Itheme {
+  bgColor: string
+  fontColor: string
+  navColor: string
+  activeBtnColor: string
+  inactiveBtnColor: string
 }
 // 2번 색을 지정해준다 => main 색들 넣어주고 다른곳에서 
 // color : ${props => props.theme.fontColor}; 이런식으로 사용하기
 export const DARKMODE = {
   bgColor: '#1D262B',
-  fontColor:'white',
-  navColor:'#275788'
-
+  fontColor: 'white',
+  navColor: '#275788',
+  activeBtnColor: '#005AB5',
+  inactiveBtnColor: '#CCCCCC',
+  containerColor: '#151B20',
 }
 export const LIGHTMODE = {
   bgColor: 'white',
-  fontColor : '#121212',
-  navColor:'#E9F2FF'
+  fontColor: '#121212',
+  navColor: '#E9F2FF',
+  activeBtnColor: '#96BDF3',
+  inactiveBtnColor: '#CCCCCC',
+  containerColor: '#F8F8F8',
 }
 
 export const themeState = atom<Itheme>({
-  key:'theme',
-  default:LIGHTMODE
+  key: 'theme',
+  default: LIGHTMODE
 })
 
 
