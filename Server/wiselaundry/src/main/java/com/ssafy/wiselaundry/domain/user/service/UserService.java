3399ba1c4bb367ce6ffdaecc10fd9c1c0d3bcab3
Haseok Jang <haseok1020@naver.com>
2022-04-25 16:36:03 +0900

    refactor/user

--- Server/wiselaundry/src/main/java/com/ssafy/wiselaundry/domain/user/service/UserService.java
+++ Server/wiselaundry/src/main/java/com/ssafy/wiselaundry/domain/user/service/UserService.java
@@ -12,4 +12,5 @@ import com.ssafy.wiselaundry.domain.user.request.UserUpdatePostReq;
 public interface UserService {
 	User createUser(UserRegisterPostReq userRegisterInfo);
 	User updateUser(UserUpdatePostReq userUpdatePostReq);
+	User findByUserId(int userId);
 }
