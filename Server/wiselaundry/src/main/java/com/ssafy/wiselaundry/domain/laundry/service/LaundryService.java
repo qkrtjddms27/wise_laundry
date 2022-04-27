5eb3f75fa6e514a942392eda10f47aa361f94c96
진은정 <1104_jin@naver.com>
2022-04-28 00:28:12 +0900

    refactor/laundry update, detail change

--- Server/wiselaundry/src/main/java/com/ssafy/wiselaundry/domain/laundry/service/LaundryService.java
+++ Server/wiselaundry/src/main/java/com/ssafy/wiselaundry/domain/laundry/service/LaundryService.java
@@ -2,6 +2,7 @@ package com.ssafy.wiselaundry.domain.laundry.service;
 
 import com.ssafy.wiselaundry.domain.laundry.db.bean.LaundryAll;
 import com.ssafy.wiselaundry.domain.laundry.db.bean.LaundryDetails;
+import com.ssafy.wiselaundry.domain.laundry.db.bean.LaundryModifys;
 import com.ssafy.wiselaundry.domain.laundry.request.LaundryModifyPostRep;
 import com.ssafy.wiselaundry.domain.laundry.request.UserLaundryRegisterPostReq;
 import org.springframework.web.multipart.MultipartHttpServletRequest;
@@ -16,5 +17,5 @@ public interface LaundryService {
     int laundryRegisterByUser(UserLaundryRegisterPostReq userLaundryRegisterPostReq, MultipartHttpServletRequest request);
     int deleteLaundry(int laundryId);
     List<LaundryAll> findLaundryAll();
-    LaundryDetails modifyLaundryDetails(LaundryModifyPostRep laundryModifyPostRep, MultipartHttpServletRequest request);
+    LaundryModifys modifyLaundryDetails(LaundryModifyPostRep laundryModifyPostRep, MultipartHttpServletRequest request);
 }
