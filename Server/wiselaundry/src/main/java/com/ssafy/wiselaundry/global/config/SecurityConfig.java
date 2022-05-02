a7bf761d2cd175af181c1ef08aa0f0540b12db4c
Haseok Jang <haseok1020@naver.com>
2022-05-03 00:04:52 +0900

    refactor/security configuration

--- Server/wiselaundry/src/main/java/com/ssafy/wiselaundry/global/config/SecurityConfig.java
+++ Server/wiselaundry/src/main/java/com/ssafy/wiselaundry/global/config/SecurityConfig.java
@@ -67,16 +67,17 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .httpBasic().disable()
-                .csrf().disable()                                                            //csrf(위조 사이트요청_보호기능을 disable, http Basic 사용하기 때문
+                .csrf().disable() //csrf(위조 사이트요청_보호기능을 disable, http Basic 사용하기 때문)
                 .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)  // 토큰 기반 인증이므로 세션 사용 하지않음
                 .and()
                 .addFilter(new JwtAuthenticationFilter(authenticationManager(),userService)) //HTTP 요청에 JWT 토큰 인증 필터를 거치도록 필터를 추가
                 .authorizeRequests()                                                         //요청에 의한 보안검사 시작
-                .antMatchers("/api/laundry/carelabel").permitAll()
-                .antMatchers("/api/laundry/all").permitAll()
-                .antMatchers("/api/laundry/**").authenticated()                   //인증이 필요한 URL과 필요하지 않은 URL에 대하여 설정
-    	        	    .anyRequest().permitAll()                                            //어떤 요청에도 보안검사를 한다.
-
+                .antMatchers("/api/user/login").permitAll() // 로그인 허용
+                .antMatchers("/api/user/signup").permitAll() // 회원가입 허용
+                .antMatchers("/api/laundry/carelabel").permitAll() // label 전체 조회 허용
+                .antMatchers("/api/laundry/all").permitAll() // laundry 전체 조회 허용
+                .antMatchers("/api/**").authenticated()  //api 로 시작하는 URL 모두 JWT 필요                  
+                .anyRequest().permitAll() // Swagger사용을 위해 모든 URL 허용
                 .and().cors();
     }
 }
