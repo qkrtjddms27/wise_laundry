d557a16e7f54ab7fd7c414218fb8c4bbfbc3b6e2
진은정 <1104_jin@naver.com>
2022-05-02 19:02:09 +0900

    fix/swagger error change

--- Server/wiselaundry/src/main/java/com/ssafy/wiselaundry/global/config/SwaggerConfig.java
+++ Server/wiselaundry/src/main/java/com/ssafy/wiselaundry/global/config/SwaggerConfig.java
@@ -2,6 +2,7 @@ package com.ssafy.wiselaundry.global.config;
 
 import org.springframework.beans.factory.annotation.Configurable;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
@@ -11,17 +12,20 @@ import springfox.documentation.spi.service.contexts.SecurityContext;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger.web.UiConfiguration;
 import springfox.documentation.swagger.web.UiConfigurationBuilder;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 import java.util.List;
 
 import static com.google.common.collect.Lists.newArrayList;
-
-@Configurable
+@EnableSwagger2
+@Configuration
 public class SwaggerConfig {
 
     @Bean
     public Docket api() {
-        return new Docket(DocumentationType.SWAGGER_2).useDefaultResponseMessages(false)
+        return new Docket(DocumentationType.SWAGGER_2)
+                .useDefaultResponseMessages(false)
+                .apiInfo(apiInfo())
                 .select()
                 .apis(RequestHandlerSelectors.any())
                 .paths(PathSelectors.ant("/api/**"))
