3ebfb8eff36ff99fc75ca115242302b83b262c81
문슬기 <tmfrlghtjr@gmail.com>
2022-05-11 02:16:09 +0900

    feature weather PopUp

--- Client/src/pages/oklaundry/Weather.tsx
+++ Client/src/pages/oklaundry/Weather.tsx
@@ -1,8 +1,49 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import styled from 'styled-components';
+import PopUpLaundry from './PopUpLaundry';
+
+
+const Wrapper = styled.div `
+  
+`
+
+const LaundryModal = styled.div `
+  /* display: flex; */
+  
+`
 
 const Weather = () => {
+  const [showPopUp, setShowPopUp] = useState(false)
+
+
+
+  useEffect(() => {
+    const popUpNotShow = sessionStorage.getItem('expirePopUp') || ''; // ISO
+    const popUpNotShowUNIX = Date.parse(popUpNotShow); // UNIX
+    const whenWillBeExpired = popUpNotShowUNIX +  24 * 60 * 60 * 1000; // 24시간(하루)
+
+    const currentUNIX = Math.floor(new Date().getTime());
+
+    // 아무 정보가 없으면 팝업을 띄우자. (처음 들어온 사람)
+    if (Number.isNaN(popUpNotShowUNIX)) {
+      setShowPopUp(true);
+    }
+
+    // 제한 시간을 지났으므로 팝업을 띄울 것
+    if (whenWillBeExpired < currentUNIX) {
+      setShowPopUp(true);
+    }
+  }, []);
+
+
+
   return (
-    <div>Weather</div>
+    <Wrapper>
+      {/* <div>Weather</div> */}
+      <LaundryModal>
+        {showPopUp && <PopUpLaundry showPopUp={showPopUp} setShowPopUp={setShowPopUp} />}
+      </LaundryModal>
+    </Wrapper>
   )
 }
 
