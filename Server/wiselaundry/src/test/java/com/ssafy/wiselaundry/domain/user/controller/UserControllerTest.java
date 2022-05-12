3c5776c666322138c5c87bcef69dcb4b6e2096dd
Haseok Jang <haseok1020@naver.com>
2022-05-12 18:09:30 +0900

    refactor/kakao image address

---
+++ Server/wiselaundry/src/test/java/com/ssafy/wiselaundry/domain/user/controller/UserControllerTest.java
@@ -0,0 +1,32 @@
+package com.ssafy.wiselaundry.domain.user.controller;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class UserControllerTest {
+
+    @Test
+    void login() {
+    }
+
+    @Test
+    void register() {
+    }
+
+    @Test
+    void update() {
+    }
+
+    @Test
+    void emailCheck() {
+    }
+
+    @Test
+    void nickCheck() {
+    }
+
+    @Test
+    void getUserInfo() {
+    }
+}
