b929bc27c01d356c78d5bbe169a3c598e3a8f0d5
bbnerino <bbnerino@gmail.com>
2022-05-13 15:10:50 +0900

    feat / weather page finish

---
+++ Client/src/pages/weather/Cloud2.tsx
@@ -0,0 +1,37 @@
+import React from 'react'
+import styled from 'styled-components'
+import './cloud2.css'
+
+const Wrapper = styled.div`
+  position: absolute;
+  overflow-y: hidden;
+  overflow-x: hidden;
+  width: 100%;
+  height: 100%;
+	z-index: 2;
+` 
+
+
+const Cloud2 = () => {
+  return (
+    <Wrapper>
+      <div className="x11">
+          <div className="cloud"/>
+      </div>
+      <div className="x21">
+          <div className="cloud"/>
+      </div>
+      <div className="x31">
+          <div className="cloud"/>
+      </div>
+      <div className="x41">
+          <div className="cloud"/>
+      </div>
+      <div className="x51">
+          <div className="cloud"/>
+      </div>
+  </Wrapper>
+  )
+}
+
+export default Cloud2
