882899d4d9f236931159fde6409a671a4e5419d0
qkrtjddms27 <qkrtjddms27@naver.com>
2022-07-27 21:19:45 +0900

    feature/ExceptionHandler 추가
    - 예외를 한 곳에서 관리하는 Exception Handler 추가
    - 예외 발생시 처리하기위한 ErrorResponse 추가
    - 에러 메시지를 한 곳에서 관리하기위해 enum 타입 ErrorCode 추가

--- Server/wiselaundry/src/main/java/com/ssafy/wiselaundry/global/model/exception/ErrorCode.java
+++ Server/wiselaundry/src/main/java/com/ssafy/wiselaundry/global/model/exception/ErrorCode.java
@@ -19,7 +19,7 @@ public enum ErrorCode {
     UNAUTHORIZED_MEMBER(UNAUTHORIZED, "현재 내 계정 정보가 존재하지 않습니다"),
 
     /* 404 NOT_FOUND : Resource 를 찾을 수 없음 */
-    MEMBER_NOT_FOUND(NOT_FOUND, "해당 유저 정보를 찾을 수 없습니다"),
+    DATA_NOT_FOUND(NOT_FOUND, "해당 데이터 정보를 찾을 수 없습니다"),
     REFRESH_TOKEN_NOT_FOUND(NOT_FOUND, "로그아웃 된 사용자입니다"),
 
     /* 409 CONFLICT : Resource 의 현재 상태와 충돌. 보통 중복된 데이터 존재 */
--- Server/wiselaundry/src/main/java/com/ssafy/wiselaundry/global/model/exception/GlobalExceptionHandler.java
+++ Server/wiselaundry/src/main/java/com/ssafy/wiselaundry/global/model/exception/GlobalExceptionHandler.java
@@ -1,2 +1,33 @@
-package com.ssafy.wiselaundry.global.model.exception;public class GlobalExceptionHandler {
+package com.ssafy.wiselaundry.global.model.exception;
+
+import com.ssafy.wiselaundry.global.model.response.ErrorResponse;
+import lombok.extern.slf4j.Slf4j;
+import org.hibernate.exception.ConstraintViolationException;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import javax.persistence.EntityNotFoundException;
+
+import static com.ssafy.wiselaundry.global.model.exception.ErrorCode.*;
+
+@Slf4j
+@RestControllerAdvice
+public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
+
+    @ExceptionHandler(value = {
+            ConstraintViolationException.class,
+            DataIntegrityViolationException.class})
+    protected ResponseEntity<ErrorResponse> handleDataDuplicateException() {
+        log.error("DataException throw Exception : {}", DUPLICATE_RESOURCE);
+        return ErrorResponse.toResponseEntity(DUPLICATE_RESOURCE);
+    }
+
+    @ExceptionHandler(value = { EntityNotFoundException.class })
+    protected ResponseEntity<ErrorResponse> handleDataNotFoundException() {
+        log.error("EntityNotFoundException throw CustomException : {}", DATA_NOT_FOUND);
+        return ErrorResponse.toResponseEntity(DATA_NOT_FOUND);
+    }
 }
